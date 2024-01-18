99c3f370d549fe32075de1a4129fc58275e79830
HackerMan888 <144242698+HackerMan888@users.noreply.github.com>
2024-01-18 14:06:53 +0000

    update comments in code

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -8,10 +8,14 @@ import os
 # 2. and the version from the Big Bang Theory, Rock-Paper-Scissors-Lizard-Spock.
 
 # searched Google for this: https://www.geeksforgeeks.org/clear-screen-python/
+# clear the screen so the game looks better
 os.system('clear')
+
+# Give the user my welcome prompts
 print('Welcome to Charlie\'s Rock-Paper-Scissors Game!\n\n')
 print('I can play two version of this game:\n')
 print('1. Classic Rock-Paper-Scissors')
 print('2. Rock-Paper-Scissors-Lizard-Spock\n')
 
+# Ask the user what version they want to play
 game_choice = input('Which version of the game would you like to play? (1, 2, quit): ')
