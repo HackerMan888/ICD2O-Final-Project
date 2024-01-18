d10ecfc8a5334bf6a12e838d297b33af8f674447
HackerMan888 <144242698+HackerMan888@users.noreply.github.com>
2024-01-18 14:03:24 +0000

    give the user the introduction and first prompt (design step 1c)

---
+++ rockpaperscissors.py
@@ -0,0 +1,17 @@
+import os
+
+# ICD2O Final Project
+# Charlie Pyke
+# Rock Paper Scissors Program
+# My project is a text version of Rock Paper Scissors. It will have two versions: 
+# 1. classic Rock-Paper-Scissors, 
+# 2. and the version from the Big Bang Theory, Rock-Paper-Scissors-Lizard-Spock.
+
+# searched Google for this: https://www.geeksforgeeks.org/clear-screen-python/
+os.system('clear')
+print('Welcome to Charlie\'s Rock-Paper-Scissors Game!\n\n')
+print('I can play two version of this game:\n')
+print('1. Classic Rock-Paper-Scissors')
+print('2. Rock-Paper-Scissors-Lizard-Spock\n')
+
+game_choice = input('Which version of the game would you like to play? (1, 2, quit): ')
