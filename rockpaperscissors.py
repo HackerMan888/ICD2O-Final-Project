8be735663f8069bfefd2110f92f45e71119c258c
HackerMan888 <144242698+HackerMan888@users.noreply.github.com>
2024-01-18 14:12:09 +0000

    I have linter errors

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -9,13 +9,13 @@ import os
 
 # searched Google for this: https://www.geeksforgeeks.org/clear-screen-python/
 # clear the screen so the game looks better
-os.system('clear')
+os.system("clear")
 
 # Give the user my welcome prompts
-print('Welcome to Charlie\'s Rock-Paper-Scissors Game!\n\n')
-print('I can play two version of this game:\n')
-print('1. Classic Rock-Paper-Scissors')
-print('2. Rock-Paper-Scissors-Lizard-Spock\n')
+print("Welcome to Charlie\'s Rock-Paper-Scissors Game!\n\n")
+print("I can play two version of this game:\n")
+print("1. Classic Rock-Paper-Scissors")
+print("2. Rock-Paper-Scissors-Lizard-Spock\n")
 
 # Ask the user what version they want to play
-game_choice = input('Which version of the game would you like to play? (1, 2, quit): ')
+game_choice = input("Which version of the game would you like to play? (1, 2, quit): ")
