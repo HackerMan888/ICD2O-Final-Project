014979758e0c7478502739ce40e82502cf236924
HackerMan888 <144242698+HackerMan888@users.noreply.github.com>
2024-01-19 13:53:07 +0000

    finish game choice prompt and start instructions prompt (task 1c and 1a)

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -17,5 +17,23 @@ print("I can play two version of this game:\n")
 print("1. Classic Rock-Paper-Scissors")
 print("2. Rock-Paper-Scissors-Lizard-Spock\n")
 
-# Ask the user what version they want to play
-game_choice = input("Which version of the game would you like to play? (1, 2, quit): ")
+# set the choice to nothing to start
+game_choice = ""
+# loop until the user gives us an acceptable input
+while (game_choice != "1" and game_choice != "2" and game_choice != "quit"):
+    # Ask the user what version they want to play
+    game_choice = input("Which version of the game would you like to play? (1, 2, quit): ")
+
+# Did the user want to quit?
+if game_choice == "quit":
+    print("\nThanks for playing!")
+    exit()
+
+# we have a game choice.
+# set the instruction prompt to nothing to start
+instructions_prompt = ""
+while (instructions_prompt != "y" and instructions_prompt != "n"):
+    # Ask the user if they want instructions
+    instructions_prompt = input("\n\nWould you like to see the rules of the game? (y, n): ")
+
+print(instructions_prompt)
